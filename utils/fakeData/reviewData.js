9cdb8c696facc81eb86085715048a297c7784c79
checkiamsiam <issiam02415@gmail.com>
2023-01-09 22:59:13 +0600

    review section data entry

---
+++ utils/fakeData/reviewData.js
@@ -0,0 +1,23 @@
+export const reviewsData = [
+  {
+    name: "Sunny Soud",
+    location: "Amman,Jordan",
+    rating: 4,
+    message:
+      "I am a marketing manager and i build a website about deferent marketplace standings situations. and they make me so happy by their friendly service. I am so glad for their. hope everyone will enjoy by work with them",
+  },
+  {
+    name: "Raskmika Khan",
+    location: "Jaipur,India",
+    rating: 4,
+    message:
+      "I build a e-commerce from this company and all of their team member was so supporting and the interesting fact is , i was gave instruction to the developers that i want this and they build this feature for me. very impressive services was that.",
+  },
+  {
+    name: "Robert Albert",
+    location: "New York,USA",
+    rating: 5,
+    message:
+      "I was needed a Mobile application for my entire website and i contact with them and they suggest me to build a native app. as per their suggestion i build a native app by them and now it reach many people and its performing well.",
+  },
+];
