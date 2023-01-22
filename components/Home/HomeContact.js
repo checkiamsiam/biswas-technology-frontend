d56a2ee290337767944cc40cf0a04f55f1898d1e
checkiamsiam <issiam02415@gmail.com>
2023-01-22 17:10:47 +0600

    hook add to the contact

--- components/Home/HomeContact.js
+++ components/Home/HomeContact.js
@@ -1,31 +1,40 @@
 import { Box, Button, Container, Grid, Stack, TextField, Typography, useTheme } from "@mui/material";
+import { useForm } from "react-hook-form";
 import style from "../../styles/home.module.css";
 
 const HomeContact = () => {
   const theme = useTheme();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <Container
       maxWidth="xl"
       className={style.setBgInContact}
       sx={{
         my: { md: 10, xs: 0 },
-        py: { md: 10, xs: 5 } ,
+        py: { md: 10, xs: 5 },
       }}
     >
       <Stack direction="row" justifyContent="center" alignItems="center">
-        <Box width={{md: "75%" , xs: "100%"}}>
+        <Box width={{ md: "75%", xs: "100%" }}>
           <Stack marginBottom={4}>
-          <Typography textAlign="center" color={theme.palette.textColorForBgSecondary.main} fontWeight="700" fontSize="15px">
-            Get In Touch
-          </Typography>
-          <Typography textAlign="center" color={theme.palette.textColorForBgSecondary.main} fontWeight="700" fontSize={{ md: "40px", xs: "25px" }}>
-            Direct Message us for any Queries
-          </Typography>
+            <Typography textAlign="center" color={theme.palette.textColorForBgSecondary.main} fontWeight="700" fontSize="15px">
+              Get In Touch
+            </Typography>
+            <Typography textAlign="center" color={theme.palette.textColorForBgSecondary.main} fontWeight="700" fontSize={{ md: "40px", xs: "25px" }}>
+              Direct Message us for any Queries
+            </Typography>
           </Stack>
-          <form>
-            <Grid container spacing={{md: 5 , xs: 2}} sx={{ marginTop: 2 }}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={{ md: 5, xs: 2 }} sx={{ marginTop: 2 }}>
               <Grid xs={12} sm={6} item>
-                <TextField sx={{ backgroundColor: theme.palette.textColorForBgSecondary.main }} placeholder="Your Name" fullWidth required />
+                <TextField
+                  sx={{ backgroundColor: theme.palette.textColorForBgSecondary.main }}
+                  placeholder="Your Name"
+                  fullWidth
+                  required
+                  {...register("name")}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -34,6 +43,7 @@ const HomeContact = () => {
                   placeholder="Enter email"
                   fullWidth
                   required
+                  {...register("email")}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -43,10 +53,17 @@ const HomeContact = () => {
                   placeholder="Enter phone number"
                   fullWidth
                   required
+                  {...register("number")}
                 />
               </Grid>
               <Grid xs={12} sm={6} item>
-                <TextField sx={{ backgroundColor: theme.palette.textColorForBgSecondary.main }} placeholder="Subject" fullWidth required />
+                <TextField
+                  sx={{ backgroundColor: theme.palette.textColorForBgSecondary.main }}
+                  placeholder="Subject"
+                  fullWidth
+                  required
+                  {...register("subject")}
+                />
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -56,6 +73,7 @@ const HomeContact = () => {
                   placeholder="Type your message here"
                   fullWidth
                   required
+                  {...register("message")}
                 />
               </Grid>
               <Grid item xs={12}>
