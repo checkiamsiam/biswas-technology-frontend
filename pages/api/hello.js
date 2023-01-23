b61280b7629a99d2f43217a7e9cf152445b33aa2
checkiamsiam <issiam02415@gmail.com>
2023-01-23 09:22:42 +0600

    mongoose setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import connectDB from "../../utils/dbConnect";
+const handler = async (req, res) => {
+  res.status(200).json({ name: "John Doe" });
+};
+export default connectDB(handler);
